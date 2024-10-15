be5ab23bb75164842fdaf87e057fb1612a53a57f
m4549071758 <m4549071758@gmail.com>
2024-10-15 20:29:53 +0900

    fix

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -10,8 +10,8 @@ import { unified } from 'unified';
 export default async function markdownToHtml(markdown: string) {
   const result = await unified()
     .use(remarkParse)
-    .use(remarkRehype)
     .use(remarkGfm)
+    .use(remarkRehype)
     .use(rehypePrism)
     .use(rehypeSlug)
     .use(rehypeAutolinkHeadings)
