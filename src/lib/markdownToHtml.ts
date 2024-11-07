6087c65407b658930a1aa4a3b038ddb53fe1ef75
m4549071758 <m4549071758@gmail.com>
2024-11-07 12:16:16 +0900

    リンクカード追加

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -4,6 +4,7 @@ import rehypePrism from 'rehype-prism-plus';
 import rehypeRaw from 'rehype-raw';
 import rehypeSlug from 'rehype-slug';
 import rehypeStringify from 'rehype-stringify';
+import rlc from 'remark-link-card';
 import remarkGfm from 'remark-gfm';
 import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
@@ -12,13 +13,14 @@ import { unified } from 'unified';
 export default async function markdownToHtml(markdown: string) {
   const result = await unified()
     .use(remarkParse)
+    .use(rlc)
     .use(remarkGfm)
     .use(remarkRehype, { allowDangerousHtml: true })
     .use(rehypeCodeTitles)
     .use(rehypePrism)
     .use(rehypeRaw)
     .use(rehypeAutolinkHeadings)
-    .use(rehypeStringify)
+    .use(rehypeStringify, { allowDangerousHtml: true })
     .use(rehypeSlug)
     .processSync(markdown);
 
