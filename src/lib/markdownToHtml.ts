5a49e0cc14d68b26a977c918c81aaf054f64dd5b
m4549071758 <m4549071758@gmail.com>
2024-11-07 12:11:37 +0900

    CSS修正

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -20,7 +20,7 @@ export default async function markdownToHtml(markdown: string) {
     .use(rehypeAutolinkHeadings)
     .use(rehypeStringify)
     .use(rehypeSlug)
-    .process(markdown);
+    .processSync(markdown);
 
   return result.toString();
 }
