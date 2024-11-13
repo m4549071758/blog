f24f8fcc85576a2d74e50bef5605bd476892feec
m4549071758 <m4549071758@gmail.com>
2024-11-13 14:04:59 +0900

    editor修正

--- src/pages/editor/index.tsx
+++ src/pages/editor/index.tsx
@@ -1,6 +1,15 @@
 import { useState } from 'react';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeCodeTitles from 'rehype-code-titles';
+import rehypeExternalLinks from 'rehype-external-links';
+import rehypePrism from 'rehype-prism-plus';
+import rehypeSlug from 'rehype-slug';
+import rehypeStringify from 'rehype-stringify';
+import remarkGfm from 'remark-gfm';
+import remarkParse from 'remark-parse';
+import remarkRehype from 'remark-rehype';
+import { unified } from 'unified';
 import { MainLayout } from '@/components/features/app/Layout';
-import markdownToHtml from '@/lib/markdownToHtml';
 
 export default function Home() {
   const [markdown, setMarkdown] = useState<string>('# Hello, world!');
@@ -13,6 +22,22 @@ export default function Home() {
     setMarkdown(newMarkdown);
     const newHtml = await markdownToHtml(newMarkdown);
     setHtml(newHtml);
+  };
+
+  const markdownToHtml = async (markdown: string) => {
+    const result = await unified()
+      .use(remarkParse)
+      .use(remarkGfm)
+      .use(remarkRehype, { allowDangerousHtml: true })
+      .use(rehypeCodeTitles)
+      .use(rehypePrism)
+      .use(rehypeAutolinkHeadings)
+      .use(rehypeExternalLinks, { target: '_blank', rel: ['nofollow'] })
+      .use(rehypeSlug)
+      .use(rehypeStringify, { allowDangerousHtml: true })
+      .process(markdown);
+
+    return result.toString();
   };
 
   return (
