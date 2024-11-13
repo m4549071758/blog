935e8934ec0fa0e624ef5ed4ae85090f7eed7bb3
m4549071758 <m4549071758@gmail.com>
2024-11-13 15:11:38 +0900

    横幅サイズ制限を解除

--- src/components/features/app/Layout/ContentLayout.tsx
+++ src/components/features/app/Layout/ContentLayout.tsx
@@ -6,7 +6,7 @@ export const ContentLayout: React.VFC<Props> = ({ children, ...props }) => {
   return (
     <Slot {...props}>
       <div className="p-6">
-        <div className="container xl:max-w-7xl mx-auto">{children}</div>
+        <div className="container mx-auto">{children}</div>
       </div>
     </Slot>
   );
