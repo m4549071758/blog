20836bbcd7323e4895c3f9f3a1cf3418d77e584c
m4549071758 <m4549071758@gmail.com>
2024-12-03 17:34:44 +0900

    delete twitter

--- src/config/sns.tsx
+++ src/config/sns.tsx
@@ -11,9 +11,4 @@ export const sns = [
     icon: <SiGithub size={20} />,
     label: 'Github',
   },
-  {
-    href: 'https://twitter.com/AAAAA67447220',
-    icon: <SiTwitter size={20} />,
-    label: 'Twitter',
-  },
 ];
