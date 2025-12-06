f5f8c22e5b45fc195dafbd50a159efa07fca4048
m4549071758 <m4549071758@gmail.com>
2025-12-06 17:20:17 +0900

    [fix]: twitterアイコンを修正

--- src/config/sns.tsx
+++ src/config/sns.tsx
@@ -1,4 +1,5 @@
 import { SiGithub, SiQiita } from 'react-icons/si';
+import { FaTwitter } from 'react-icons/fa';
 
 export const sns = [
   {
@@ -10,5 +11,10 @@ export const sns = [
     href: 'https://github.com/m4549071758',
     icon: <SiGithub size={20} />,
     label: 'Github',
+  },
+  {
+    href: 'https://twitter.com/katori_m',
+    icon: <FaTwitter size={20} />,
+    label: 'Twitter',
   },
 ];
