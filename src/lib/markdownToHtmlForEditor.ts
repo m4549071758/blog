a023a562e184ee50c83520f671e45610959bf99b
m4549071758 <m4549071758@gmail.com>
2026-04-25 02:13:31 +0900

    [src] editor

--- src/lib/markdownToHtmlForEditor.ts
+++ src/lib/markdownToHtmlForEditor.ts
@@ -5,10 +5,8 @@ import rehypeSlug from 'rehype-slug';
 import rehypeStringify from 'rehype-stringify';
 import rehypeSanitize, { defaultSchema } from 'rehype-sanitize';
 import rehypeShiki from '@shikijs/rehype';
-import rehypeRaw from 'rehype-raw';
 import remarkBreaks from 'remark-breaks';
 import remarkGfm from 'remark-gfm';
-import rlc from 'remark-link-card';
 import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
 import remarkYoutube from 'remark-youtube';
@@ -20,10 +18,8 @@ export default async function markdownToHtmlForEditor(markdown: string) {
     .use(remarkParse)
     .use(remarkBreaks)
     .use(remarkGfm as any)
-    .use(rlc, { downloadLimit: 10000000 })
     .use(remarkYoutube as any)
     .use(remarkRehype, { allowDangerousHtml: true })
-    .use(rehypeRaw)
     .use(rehypeCodeTitles)
     .use(rehypeShiki, {
       theme: 'github-dark',
@@ -44,11 +40,9 @@ export default async function markdownToHtmlForEditor(markdown: string) {
           'scrolling',
         ],
         code: [['className', /^language-./]],
-        span: [...(defaultSchema.attributes?.span || []), ['className', /^token$/, /^rlc-./], 'style'],
-        div: [...(defaultSchema.attributes?.div || []), ['className', 'rehype-code-title', /^rlc-./]],
-        pre: [...(defaultSchema.attributes?.pre || []), 'style', ['className', 'shiki', /^language-./]],
-        a: [...(defaultSchema.attributes?.a || []), ['className', /^rlc-./], 'href', 'target', 'rel'],
-        img: [...(defaultSchema.attributes?.img || []), ['className', /^rlc-./], 'src', 'alt', 'loading'],
+        span: [['className', /^token$/], 'style'],
+        div: [['className', 'rehype-code-title']],
+        pre: ['style', ['className', 'shiki', /^language-./]],
       },
     })
     .use(rehypeAutolinkHeadings)
